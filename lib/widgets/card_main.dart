bc054a97ba13543dc1d667b646799b80dc174245
Okeke Fabian <freebornprince@yahoo.com>
2022-03-02 16:29:21 +0100

    first commit

---
+++ lib/widgets/card_main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:stress_app/screens/detail_screen.dart';
+import 'package:stress_app/utils/const.dart';
+
+import 'custom_clipper.dart';
+
+class CardMain extends StatelessWidget {
+  final ImageProvider image;
+  final String title;
+  final String value;
+  final String unit;
+  final Color color;
+
+  CardMain(
+      {required this.image,
+      required this.title,
+      required this.value,
+      required this.unit,
+      required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Container(
+        margin: const EdgeInsets.only(right: 15.0),
+        width: ((MediaQuery.of(context).size.width -
+                (Constants.paddingSide * 2 + Constants.paddingSide / 2)) /
+            2),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+          shape: BoxShape.rectangle,
+          color: color,
+        ),
+        child: Material(
+          child: InkWell(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            child: Stack(
+              children: [
+                Positioned(
+                  child: ClipPath(
+                    clipper: MyCustomClipper(clipType: ClipType.semiCircle),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10.0)),
+                        color: Colors.black.withOpacity(0.03),
+                      ),
+                      height: 120,
+                      width: 120,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Icon and Hearbeat
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image(width: 35, height: 35, image: image),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              title,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontSize: 13, color: Constants.textDark),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        value,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w900,
+                          color: Constants.textDark,
+                        ),
+                      ),
+                      Text(
+                        unit,
+                        style:
+                            TextStyle(fontSize: 15, color: Constants.textDark),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            onTap: () {
+              debugPrint("CARD main clicked. redirect to details page");
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailScreen()),
+              );
+            },
+          ),
+          color: Colors.transparent,
+        ),
+      ),
+    );
+  }
+}
